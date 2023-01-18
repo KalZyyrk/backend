03a1b817aed159c63e1d3fd4aacaa7a6366f65cb
Ulysse Hischke <u.hischke@outlook.fr>
2023-01-18 12:27:47 +0100

    fin

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const auth = require('../middleware/auth')
 const router = express.Router();
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-const Thing = require("../models/Thing")
+const Thing = require("../models/Thing");
 
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', auth, stuffCtrl.createThing);
-
-router.put('/:id', auth, stuffCtrl.modifyThing);
-
-router.delete('/:id', auth, stuffCtrl.deleteThing)
-
-router.get('/:id', auth, stuffCtrl.getOneThing);
-
 router.get('/', auth, stuffCtrl.getAllThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+
 
 module.exports = router;
